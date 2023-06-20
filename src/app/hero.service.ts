ac75012fa9b8cad907632326e1271242f055e183
Javier Lete <javier@eskura21.com>
2023-06-20 19:45:37 +0200

    Cambiamos el HeroService para utilizar REST

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,25 +1,24 @@
 import { Injectable } from '@angular/core';
-import { HEROES } from './mock-heroes';
 import { Hero } from './hero';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
-  
-  constructor(private messageService: MessageService) { }
+  private heroesUrl = 'http://localhost:3000/heroes/';
+
+  constructor(private messageService: MessageService, private http: HttpClient) { }
   
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
     this.messageService.add('HeroService: fetched heroes');
-    return heroes;
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
   
   getHero(id: number): Observable<Hero> {
-    const hero: Hero = HEROES.find(hero => hero.id === id)!;
     this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(hero);
+    return this.http.get<Hero>(this.heroesUrl + id);
   }
 }
