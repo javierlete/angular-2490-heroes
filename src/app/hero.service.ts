b8e7230c6c334162e951fb1ae23022d1e333aadd
Javier Lete <javier@eskura21.com>
2023-06-21 20:09:40 +0200

    Control de errores de servicio REST

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
-import { Observable, of } from 'rxjs';
+import { Observable, catchError, of, tap } from 'rxjs';
 import { MessageService } from './message.service';
 import { HttpClient } from '@angular/common/http';
 
@@ -11,15 +11,25 @@ export class HeroService {
   private heroesUrl = 'http://localhost:3000/heroes/';
 
   constructor(private messageService: MessageService, private http: HttpClient) { }
-  
+
   getHeroes(): Observable<Hero[]> {
-    this.messageService.add('HeroService: fetched heroes');
-    return this.http.get<Hero[]>(this.heroesUrl);
+    return this.http.get<Hero[]>(this.heroesUrl).pipe(
+      tap(_ => this.log('fetched heroes')),
+      catchError(this.handleError<Hero[]>('getHeroes', []))
+      //tap(heroes => this.messageService.add(`HeroService: fetched ${heroes.length} heroes`)),
+      //catchError(() => {
+      //  this.messageService.add('FAILED TO FETCH HEROES');
+      //  return of([]);
+      //})
+    );
   }
-  
+
   getHero(id: number): Observable<Hero> {
     this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return this.http.get<Hero>(this.heroesUrl + id);
+    return this.http.get<Hero>(this.heroesUrl + id).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
   }
 
   searchHeroes(term: string): Observable<Hero[]> {
@@ -29,20 +39,49 @@ export class HeroService {
     }
 
     this.messageService.add(`HeroService: fetched heroes matching "${term}"`);
-    return this.http.get<Hero[]>(`${this.heroesUrl}?name_like=${term}`);
+    return this.http.get<Hero[]>(`${this.heroesUrl}?name_like=${term}`).pipe(
+      tap(x => x.length ?
+         this.log(`found heroes matching "${term}"`) :
+         this.log(`no heroes matching "${term}"`)),
+      catchError(this.handleError<Hero[]>('searchHeroes', []))
+    );
   }
 
   addHero(hero: Hero) {
     this.messageService.add(`HeroService: added hero ${hero.name}`);
-    return this.http.post<Hero>(this.heroesUrl, hero);
+    return this.http.post<Hero>(this.heroesUrl, hero).pipe(
+      tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
   }
   updateHero(hero: Hero): Observable<Hero> {
     this.messageService.add(`HeroService: updated hero id=${hero.id}`);
-    return this.http.put<Hero>(this.heroesUrl + hero.id, hero);
+    return this.http.put<Hero>(this.heroesUrl + hero.id, hero).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    );
   }
 
   deleteHero(id: number): Observable<any> {
     this.messageService.add(`HeroService: deleting hero id=${id}`);
-    return this.http.delete<Hero>(this.heroesUrl + id);
+    return this.http.delete<Hero>(this.heroesUrl + id).pipe(
+      tap(_ => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
+  }
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+  
+      console.error(error); // log to console instead
+  
+      this.log(`${operation} failed: ${error.message}`);
+  
+      return of(result as T);
+    };
   }
 }
